f9c511021bd9523ef3b404c80a99aaf573e5a993
XBxiaoXB <BXxiao19961224>
2022-09-16 00:32:21 +0900

    1.2

--- rimokonn/timerA.c
+++ rimokonn/timerA.c
@@ -5,7 +5,7 @@
 
 void iniTimerA(void)
 {
-	TA.TMA.BYTE = 0x1A;     //shiji 0.25s
+	TA.TMA.BYTE = 0x10;     //shiji 0.25s
 	IRR1.BIT.IRRTA = 0;   //chushihua
 	IENR1.BIT.IENTA = 0;  //jinzhizhongduan
 }
